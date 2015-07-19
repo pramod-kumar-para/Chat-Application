35c832e297f87d86cb0444d41f5fe9bcfb5406fe
pramod-kumar-para <pramodkumarpara95@gmail.com>
2015-07-19 12:45:43 +0530

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -1,22 +1,24 @@
 package Client_Server_Chat;
-import java.io.*;
 import java.net.*;
-public class MyClient {
+import java.io.*;
+public class MyServer {
 	public static void main(String[] args)throws Exception {
 		// TODO Auto-generated method stub
-		Socket s=new Socket("localhost",6666);
-		DataInputStream din=new DataInputStream(s.getInputStream());  
-		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-		String str="",str2="";  
-		while(!str.equals("stop")){  
-		str=br.readLine();  
-		dout.writeUTF(str);  
-		dout.flush();  
-		str2=din.readUTF();  
-		System.out.println("Server says: "+str2);  
-		}  
-		dout.close();  
-		s.close();  	
+		ServerSocket s=new ServerSocket(6666);
+		Socket sock=s.accept();
+		DataInputStream dis=new DataInputStream(sock.getInputStream());
+		DataOutputStream dout=new DataOutputStream(sock.getOutputStream());
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String str="",str2="";
+		while(str!="stop") {
+			str=dis.readUTF();
+			System.out.println("Client says: "+str);
+			str2=br.readLine();
+			dout.writeUTF(str2);
+			dout.flush();
+			}
+			dis.close();
+			sock.close();
+			s.close();
 	}
 }
