a0ad0eb1ddb52c1abbef41c4ab0a6e1533db8960
pramod-kumar-para <pramodkumarpara95@gmail.com>
2015-07-19 12:44:57 +0530

    Create MyServer.java

---
+++ MyServer.java
@@ -0,0 +1,22 @@
+package Client_Server_Chat;
+import java.io.*;
+import java.net.*;
+public class MyClient {
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		Socket s=new Socket("localhost",6666);
+		DataInputStream din=new DataInputStream(s.getInputStream());  
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+		String str="",str2="";  
+		while(!str.equals("stop")){  
+		str=br.readLine();  
+		dout.writeUTF(str);  
+		dout.flush();  
+		str2=din.readUTF();  
+		System.out.println("Server says: "+str2);  
+		}  
+		dout.close();  
+		s.close();  	
+	}
+}
